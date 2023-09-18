fa819e3b2a3f6e2a17125d1787740f92d1117784
Паламар Роман <romapalamar.veiag@gmail.com>
2023-09-18 21:17:52 +0300

    fixed add mobile

--- src/components/modals/pages/add-book/color-settings-page.js
+++ src/components/modals/pages/add-book/color-settings-page.js
@@ -3,8 +3,10 @@ import React from "react"
 import { ColorPicker } from "react-color-palette";
 import "react-color-palette/css";
 const ColorSettingsPage = ({color , setColor})=>{
+
+    const mq = window.matchMedia("(max-width: 735px)");
     return <div className="color-picker">
-        <ColorPicker color={color} onChange={setColor} hideAlpha={true} />
+        <ColorPicker color={color} onChange={setColor} hideAlpha={true} hideInput={mq.matches ? [true,false ,false] : false}/>
     </div>
 }
 export default ColorSettingsPage;
